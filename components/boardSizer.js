f901cfff41e89ba044f97dbbf988e9ce6a9ccf6e
youngrichard <to.richardyoung@gmail.com>
2020-04-19 19:46:41 -0500

    Rename boardSizer

--- components/boardSizer.js
+++ components/boardSizer.js
@@ -1,16 +1,22 @@
-import gameActionTypes from '../actions/gameActions';
+import GameActionTypes from '../actions/gameActions';
 
 import { BOARD_SIZE_MINE_COUNT_MAP } from '../constants/gameConstants';
 import { useGameState, useGameDispatch } from '../contexts/gameContext';
 
-const Resizer = () => {
+const BoardSizer = () => {
   const { boardSize } = useGameState();
   const dispatch = useGameDispatch();
 
-  const onChangeHandler = event => dispatch({
-    type: gameActionTypes.changeBoardSize,
-    payload: event.target.value,
-  });
+  const onChangeHandler = event => {
+    dispatch({
+      type: GameActionTypes.CHANGE_BOARD_SIZE,
+      payload: Number(event.target.value),
+    });
+
+    dispatch({
+      type: GameActionTypes.RESET_BOARD,
+    })
+  }
 
   return (
     <select value={boardSize} onChange={onChangeHandler}>
@@ -19,4 +25,4 @@ const Resizer = () => {
   );
 };
 
-export default Resizer;
+export default BoardSizer;
